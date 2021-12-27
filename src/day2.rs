45a4ef4963a7943f5198212ad2445fe23968e450
cyberia <cyberia@donk.systems>
2021-12-27 21:28:30 +0000

    day 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,3 +1,70 @@
+use anyhow::anyhow;
+use std::str::FromStr;
+
+enum Direction {
+    Forward,
+    Down,
+    Up,
+}
+use Direction::*;
+
+struct Command {
+    direction: Direction,
+    amount: u32,
+}
+
+impl FromStr for Command {
+    type Err = anyhow::Error;
+
+    fn from_str(line: &str) -> Result<Self, Self::Err> {
+        let (direction, amount) = line
+            .split_once(' ')
+            .ok_or_else(|| anyhow!("Invalid command: {}", line))?;
+        let direction = match direction {
+            "forward" => Forward,
+            "down" => Down,
+            "up" => Up,
+            _ => return Err(anyhow!("Bad direction: {}", direction)),
+        };
+        let amount = amount.parse()?;
+        Ok(Command { direction, amount })
+    }
+}
+
 pub fn part1(input: &str) -> anyhow::Result<String> {
-    todo!()
+    let commands: Box<[Command]> = input
+        .lines()
+        .map(|line| line.parse())
+        .collect::<Result<_, _>>()?;
+    let mut horizontal = 0u32;
+    let mut depth = 0u32;
+    for command in commands.iter() {
+        match command.direction {
+            Forward => horizontal += command.amount,
+            Down => depth += command.amount,
+            Up => depth -= command.amount,
+        }
+    }
+    Ok(format!("{}", horizontal * depth))
+}
+
+pub fn part2(input: &str) -> anyhow::Result<String> {
+    let commands: Box<[Command]> = input
+        .lines()
+        .map(|line| line.parse())
+        .collect::<Result<_, _>>()?;
+    let mut horizontal = 0u32;
+    let mut depth = 0u32;
+    let mut aim = 0i32;
+    for command in commands.iter() {
+        match command.direction {
+            Down => aim += command.amount as i32,
+            Up => aim -= command.amount as i32,
+            Forward => {
+                horizontal += command.amount;
+                depth += (command.amount as i32 * aim) as u32;
+            }
+        }
+    }
+    Ok(format!("{}", horizontal * depth))
 }
--- src/main.rs
+++ src/main.rs
@@ -24,13 +24,13 @@ fn main() -> anyhow::Result<()> {
         (1, 1) => day1::part1,
         (1, 2) => day1::part2,
         (2, 1) => day2::part1,
-        _ => Err(anyhow!("Invalid day/part"))?,
+        (2, 2) => day2::part2,
+        _ => return Err(anyhow!("Invalid day/part")),
     });
     eprintln!("Reading input");
     let mut input = Vec::new();
     stdin().read_to_end(&mut input)?;
     let input = String::from_utf8(input)?;
-
 
     let output = part_fn(&input)?;
     println!("{}", output);
