fd94b47182542ef36052245ee7be5a652bab575d
cyberia <cyberia@donk.systems>
2021-12-27 20:48:34 +0000

    improve top level switching

---
+++ src/day2.rs
@@ -0,0 +1,3 @@
+pub fn part1(input: &str) -> anyhow::Result<String> {
+    todo!()
+}
--- src/main.rs
+++ src/main.rs
@@ -5,36 +5,34 @@ use std::{
 };
 
 mod day1;
+mod day2;
 
 fn main() -> anyhow::Result<()> {
-    let days: Vec<[Box<dyn Fn(&str) -> anyhow::Result<String>>; 2]> =
-        vec![[Box::new(day1::part1), Box::new(day1::part2)]];
-
     let mut args = args();
     args.next();
     let day = args
         .next()
         .ok_or(anyhow!("Must provide day number argument"))?;
     let day = day.parse::<usize>()?;
-    let day_index = day.checked_sub(1).ok_or(anyhow!("Days start at 1"))?;
     let part = args
         .next()
         .ok_or(anyhow!("Must provide part number argument"))?;
     let part = part.parse::<usize>()?;
-    let part_index = part.checked_sub(1).ok_or(anyhow!("Parts start at 1"))?;
 
-    let day_fn = days
-        .get(day_index)
-        .ok_or(anyhow!("No such day"))?
-        .get(part_index)
-        .ok_or(anyhow!("No such part"))?;
-
+    type PartFn = dyn Fn(&str) -> anyhow::Result<String>;
+    let part_fn: Box<PartFn> = Box::new(match (day, part) {
+        (1, 1) => day1::part1,
+        (1, 2) => day1::part2,
+        (2, 1) => day2::part1,
+        _ => Err(anyhow!("Invalid day/part"))?,
+    });
     eprintln!("Reading input");
     let mut input = Vec::new();
     stdin().read_to_end(&mut input)?;
     let input = String::from_utf8(input)?;
 
-    let output = day_fn(&input)?;
+
+    let output = part_fn(&input)?;
     println!("{}", output);
     Ok(())
 }
